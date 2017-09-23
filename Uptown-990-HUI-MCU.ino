5479d6dbc2e9f6abb6931d9e089ab4ff903b7eb4
vjmuzik <vjmuzik1@gmail.com>
2017-09-23 15:57:46 -0400

    Added functionality for Uptown 990 Control Panel
    Auto, Ready LEDs are always on after initializing, unless disconnected
    Hold, Manual, Touch, Write LEDs are always off after initializing
    
    Can't be changed since the driver board functionality changes with it and it always has to be in Ready Mode with Touch and Hold off for it to work with the DAW
    
    Buttons work independently from the LEDs
    
    S1, S2 LEDs change with the DAW
    
    Control Panel Button Mappings for HUI/ MCU
    S1 = Undo
    S2 = Save
    Hold = Global Automation Latch
    Manual = Global Automation Off(HUI), Global Automation Read/Off(MCU)
    Auto = Global Automation Read(HUI), Global Automation Read/Off(MCU)
    Ready = Global Automation Trim
    Touch = Global Automation Touch
    Write = Global Automation Write

--- Uptown-990-HUI-MCU.ino
+++ Uptown-990-HUI-MCU.ino
@@ -5,10 +5,12 @@
 
 #define uptReadLen 20          //Number of bytes to read from uptown driver board
 #define uptWriteLen 21         //Number of bytes to write to uptown driver board
+uint8_t uptCurrentMode;         //0 = manual, 1 = auto, 2 = ready, 3 = write  Not used
 uint8_t uptRead[uptReadLen];      //Buffer for data read from uptown driver board
 uint8_t uptWrite[uptWriteLen];    //Buffer for data written to uptown driver board
 uint16_t uptFaderRead[8];         //Buffer for read fader positions
 uint8_t uptButtonRead[8];         //Buffer for read button states
+uint8_t uptControlPanelRead[8];   //Buffer for read control panel button states
 uint8_t uptTouchRead[8];          //Buffer for read touch states
 uint16_t uptFaderWrite[8];        //Buffer for written fader positions
 uint8_t uptButtonWrite[8];        //Buffer for written button states
@@ -18,8 +20,12 @@ const uint8_t faderMSBAddress[8] = {9, 8, 7, 6, 5, 4, 3, 2};         //Address f
 const uint8_t faderLSBPosition[8] = {6, 4, 2, 0, 6, 4, 2, 0};
 const uint8_t faderLSBAddress[8] = {20, 20, 20, 20, 19, 19, 19, 19};
 uint8_t muteLastState[8];          //Used to check button state, toggle on change from 0x10 to 0x08, toggle off on change from 0x08 to 0x10
+uint8_t controlPanelLastState[8];
 uint8_t touchLastState[8];
 uint16_t faderLastValue[8];
+const uint8_t controlPanelZone[8] = {0x08, 0x08, 0x18, 0x18, 0x18, 0x18, 0x18, 0x18};
+const uint8_t controlPanelPort[8] = {0x03, 0x07, 0x03, 0x02, 0x00, 0x04, 0x05, 0x01};
+const uint8_t controlPanelNote[8] = {0x51, 0x50, 0x4A, 0x4A, 0x4C, 0x4B, 0x4D, 0x4E};
 const uint8_t addressConverter[8] = {0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
 elapsedMillis driverIO;
 
@@ -47,8 +53,6 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) {
     serialRead();
-    delay(40);
-    uptRunMode();
   }
   while(usbMIDI.read()){
   
@@ -59,6 +63,10 @@ void loop() {
   
   if (driverIO >= 30) {
     driverIO = driverIO - 30;
+    if(driverIO > 0) {
+      Serial.print("Latency: ");
+      Serial.println(driverIO);
+    }
     uptReadFrom();
     uptWriteTo();
   }
@@ -98,6 +106,48 @@ void serialRead() {
   }
   else if (serial == "position") {
     uptPosition();
+  }
+  else if (serial == "runMode") {
+    uptRunMode();
+  }
+  else if (serial == "allOff") {
+    uptWrite[0] = 0x00;
+  }
+  else if (serial == "autoOn") {
+    uptAutoOn();
+  }
+  else if (serial == "manualOn") {
+    uptManualOn();
+  }
+  else if (serial == "readyOn") {
+    uptReadyOn();
+  }
+  else if (serial == "writeOn") {
+    uptWriteOn();
+  }
+  else if (serial == "touchOn") {
+    uptTouchOn();
+  }
+  else if (serial == "touchOff") {
+    uptTouchOff();
+  }
+  else if (serial == "holdOn") {
+    uptHoldOn();
+  }
+  else if (serial == "holdOff") {
+    uptHoldOff();
+  }
+  else if (serial == "S1On") {
+    uptS1On();
+  }
+  else if (serial == "S1Off") {
+    uptS1Off();
+  }
+  else if (serial == "S2On") {
+    uptS2On();
+  }
+  else if (serial == "S2Off") {
+    uptS2Off();
   }
   else {
     Serial.println("Not a valid command");
@@ -225,7 +275,7 @@ void uptRunMode() {
   delay(40);
   uptMode(0x90, 0x90);
   delay(15);
-  uptWrite[0] = 0x90;
+  uptWrite[0] = 0x00;
   uptWrite[1] = 0x08;
 }
 
@@ -710,6 +760,58 @@ void uptPosition() {
   }
 }
 
+void uptTouchOn() {
+  uptMode(0xE1, 0xE1);
+}
+
+void uptTouchOff() {
+  uptMode(0xE2, 0xE2);
+}
+
+void uptHoldOn() {
+  uptMode(0x34, 0x34);
+}
+
+void uptHoldOff() {
+  uptMode(0x33, 0x33);
+}
+
+void uptAutoOn() {
+  uptMode(0x20, 0x20);
+  uptCurrentMode = 1;
+}
+
+void uptReadyOn() {
+  uptMode(0x90, 0x90);
+  uptCurrentMode = 2;
+}
+
+void uptWriteOn() {
+  uptMode(0xE0, 0xE0);
+  uptCurrentMode = 3;
+}
+
+void uptManualOn() {
+  uptMode(0xF0, 0xF0);
+  uptCurrentMode = 0;
+}
+
+void uptS1On() {
+  uptWrite[0] = uptWrite[0] | 0x01;
+}
+
+void uptS2On() {
+  uptWrite[0] = uptWrite[0] | 0x02;
+}
+
+void uptS1Off() {
+  uptWrite[0] = uptWrite[0] & 0xFE;
+}
+
+void uptS2Off() {
+  uptWrite[0] = uptWrite[0] & 0xFD;
+}
+
 void uptSetupWriteButtons() {
   for(int c = 0; c < 8; c++) {
     uptWrite[muteAddress[c]] = uptButtonWrite[c];
@@ -720,6 +822,7 @@ void uptSetupReadButtons() {
   for(int c = 0; c < 8; c++) {
     uptButtonRead[c] = uptRead[muteAddress[c] - 2];
     uptTouchRead[c] = uptButtonRead[c] >> 5;
+    uptControlPanelRead[c] = (uptRead[16] >> c) & 0x01;
     uptButtonRead[c] = uptButtonRead[c] & 0x1F;
     if(uptButtonRead[c] != muteLastState[c]) {
       if((uptButtonRead[c] == 0x10 && muteLastState[c] == 0x00) | (uptButtonRead[c] == 0x08 && muteLastState[c] == 0x00)) {
@@ -747,6 +850,30 @@ void uptSetupReadButtons() {
         usbMIDI.sendNoteOn(0x60 | addressConverter[c], 0x00, 1);
       }
       touchLastState[c] = uptTouchRead[c];
+    }
+
+    if(uptControlPanelRead[c] != controlPanelLastState[c]) {
+      if(uptControlPanelRead[c] == 0x01 && controlPanelLastState[c] == 0x00) {
+        if(c > 0x01) {
+          usbMIDI.sendControlChange(0x0F, 0x08, 1);
+          usbMIDI.sendControlChange(0x2F, 0x40 | 0x05, 1);
+          usbMIDI.sendNoteOn(0x47, 0x7f, 1);
+        }
+        usbMIDI.sendControlChange(0x0F, controlPanelZone[c], 1);
+        usbMIDI.sendControlChange(0x2F, 0x40 | controlPanelPort[c], 1);
+        usbMIDI.sendNoteOn(controlPanelNote[c], 0x7f, 1);
+      }
+      else if(controlPanelLastState[c] == 0x01 && uptControlPanelRead[c] == 0x00) {
+        usbMIDI.sendControlChange(0x0F, controlPanelZone[c], 1);
+        usbMIDI.sendControlChange(0x2F, 0x00 | controlPanelPort[c], 1);
+        usbMIDI.sendNoteOn(controlPanelNote[c], 0x00, 1);
+        if(c > 0x01) {
+          usbMIDI.sendControlChange(0x0F, 0x08, 1);
+          usbMIDI.sendControlChange(0x2F, 0x00 | 0x05, 1);
+          usbMIDI.sendNoteOn(0x47, 0x00, 1);
+        }
+      }
+      controlPanelLastState[c] = uptControlPanelRead[c];
     }
   }
 }
@@ -778,7 +905,7 @@ void uptSetupReadFaders() {
       uptFaderRead[f] = 0;
     }
     if(uptTouchRead[f] == 0x01 && (uptFaderRead[f]) != faderLastValue[f]){
-      Serial.println(uptFaderRead[f]);
+//      Serial.println(uptFaderRead[f]);
       usbMIDI.sendPitchBend(uptFaderRead[f] << 4, f + 1);
       usbMIDI.sendControlChange(0x00 | f, uptFaderRead[f] >> 3, 1);
       usbMIDI.sendControlChange(0x20 | f, (uptFaderRead[f] << 4) & 0x7F, 1);
@@ -798,6 +925,16 @@ void OnNoteOn(uint8_t channel, uint8_t note, uint8_t velocity) {
         uptButtonWrite[note & 0x0F] = 0x01;
        }
     }
+    else if(note == 0x51) {
+       if(velocity == 0x7F){
+        uptS1On();
+       }
+    }
+    else if(note == 0x50) {
+       if(velocity == 0x7F){
+        uptS2On();
+       }
+    }
   }
   
 }
@@ -812,14 +949,24 @@ void OnNoteOff(uint8_t channel, uint8_t note, uint8_t velocity) {
         uptButtonWrite[note & 0x0F] = 0x00;
        }
     }
+    else if(note == 0x51) {
+       if(velocity == 0x00){
+        uptS1Off();
+       }
+    }
+    else if(note == 0x50) {
+       if(velocity == 0x00){
+        uptS2Off();
+       }
+    }
   }
 }
 
 void OnControlChange(uint8_t channel, uint8_t control, uint8_t value) {
-  if(channel == 1 && control == 0x0c) {
+  if(channel == 1 && control == 0x0C) {
     zoneSelect = value;
   }
-  else if(channel == 1 && control == 0x2c && zoneSelect < 8) {
+  else if(channel == 1 && control == 0x2C && zoneSelect < 0x08) {
     if((value >> 4) == 0x04) {
       value = value & 0x0F;
       if(value == 0x02) {
@@ -832,6 +979,26 @@ void OnControlChange(uint8_t channel, uint8_t control, uint8_t value) {
         uptButtonWrite[zoneSelect] = 0x00;
       }
       
+    }
+  }
+  else if(channel == 1 && control == 0x2C && zoneSelect == 0x08) {
+    if((value >> 4) == 0x04) {
+      value = value & 0x0F;
+      if(value == 0x03) {
+        uptS1On();
+      }
+      else if(value == 0x07) {
+        uptS2On();
+      }
+    }
+    else if((value >> 4) == 0x00) {
+      value = value & 0x0F;
+      if(value == 0x03) {
+        uptS1Off();
+      }
+      else if(value == 0x07) {
+        uptS2Off();
+      }
     }
   }
   else if(channel == 1 && (control >= 0x00 && control <= 0x07)) {
